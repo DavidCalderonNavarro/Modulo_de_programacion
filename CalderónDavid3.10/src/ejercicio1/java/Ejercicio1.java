3980e589e36aaa8da485582186399e723e7583df
David Calderón <dcalnav0907@g.educaand.es>
2025-10-22 21:22:22 +0200

    Tarea 3.10

---
+++ CalderónDavid3.10/src/ejercicio1/java/Ejercicio1.java
@@ -0,0 +1,22 @@
+package ejercicio1.java;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner datos = new Scanner(System.in);
+		
+		//int num[] = new n
+		
+		for (int contador = 0; contador <= 10;contador++) {
+			
+			System.out.print("Dame un numero: ");
+			
+			
+		}
+		
+	}
+
+}
